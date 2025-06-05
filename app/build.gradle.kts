5261772a51448a32f9c3983b5b44f67986671ada
vpk11 <devvpk@gmail.com>
2025-06-05 23:27:50 +0200

    :sparkles: feat: use kotlin instead and upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
